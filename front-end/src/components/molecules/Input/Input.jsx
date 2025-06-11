a15362490bf2b2b118469d0dbf0a2a76362005dc
Jonas Felix <jonaspetty@hotmail.com>
2025-06-11 16:14:30 +0000

    feat: add the full input component

---
+++ front-end/src/components/molecules/Input/Input.jsx
@@ -0,0 +1,21 @@
+import Label from "../../atoms/Labels/Label/Label";
+import Input from "../../atoms/Inputs/Input/Input";
+
+import "./Input.css"
+
+function LabeledInput(props) {
+    return (
+        <div className="inputContainer">
+            <Label
+                for={props.id}
+                text={props.label}
+            />
+            <Input
+                type={props.type}
+                placeholder={props.placeholder}
+            />
+        </div>
+    );
+}
+
+export default LabeledInput;
