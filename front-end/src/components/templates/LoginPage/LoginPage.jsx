ff041efb2fb8a5f6fd6a74025b6bbffaa0ffaca8
Jonas Felix <jonaspetty@hotmail.com>
2025-06-12 01:20:26 +0000

    fix: fix components styles

---
+++ front-end/src/components/templates/LoginPage/LoginPage.jsx
@@ -0,0 +1,13 @@
+import LoginForm from "../../organisms/Forms/LoginForm/LoginForm"
+
+import "./LoginPage.css"
+
+function LoginPage(props) {
+    return (
+        <>
+            <LoginForm/>
+        </>
+    );
+}
+
+export default LoginPage;
