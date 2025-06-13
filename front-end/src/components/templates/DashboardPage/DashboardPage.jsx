dc27b60fd6197d7ce76f5848875e3672f630ffcc
Jonas Felix <jonaspetty@hotmail.com>
2025-06-13 03:55:55 +0000

    feat: add basic DashboardPage planing

--- front-end/src/components/templates/DashboardPage/DashboardPage.jsx
+++ front-end/src/components/templates/DashboardPage/DashboardPage.jsx
@@ -2,7 +2,22 @@ import "./DashboardPage.css"
 
 function DashboardPage(props) {
     return ( 
-        <h1>dashboard</h1>
+        <div className="dashboard-container">
+            <div className="sidebar">
+                sidebar
+            </div>
+            <div className="main-content-container">
+                <div className="header">
+                    header
+                </div>
+                <div className="main-content">
+                    
+                    <div className="todo">todo</div>
+                    <div className="doing">doing</div>
+                    <div className="done">done</div>
+                </div>
+            </div>
+        </div>
      );
 }
 
