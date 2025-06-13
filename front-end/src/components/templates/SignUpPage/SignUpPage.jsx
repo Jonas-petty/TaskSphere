0a8b5680b06c1f52e7d62fdad1823dda61870ce2
Jonas Felix <jonaspetty@hotmail.com>
2025-06-13 00:17:02 +0000

    feat: add the SignUpPage component

---
+++ front-end/src/components/templates/SignUpPage/SignUpPage.jsx
@@ -0,0 +1,13 @@
+import SignUpForm from "../../organisms/Forms/SignUpForm/SignUpForm"
+
+import "./SignUpPage.css"
+
+function SignUpPage(props) {
+    return ( 
+        <div className="container">
+            <SignUpForm />
+        </div>
+     );
+}
+
+export default SignUpPage;
