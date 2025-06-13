bbe0e3037e6c18470a66b41a1b20c1680c5e6953
Jonas Felix <jonaspetty@hotmail.com>
2025-06-13 00:35:37 +0000

    feat: add the SignUpForm submit handling

--- front-end/src/components/organisms/Forms/SignUpForm/SignUpForm.jsx
+++ front-end/src/components/organisms/Forms/SignUpForm/SignUpForm.jsx
@@ -2,17 +2,49 @@ import LabeledInput from "../../../molecules/Input/Input";
 import DefaultButton from "../../../atoms/Buttons/Default/DefaultButton";
 
 import "./SignUpForm.css";
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
 function SignUpForm(props) {
+    const navigate = useNavigate()
+
+    const [userData, setUserData] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+
+    function handleChange(field, value) {
+        setUserData((prev) => ({ ...prev, [field]: value }));
+    }
+
+    function handleSubmit(e) {
+        const options = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(userData),
+        };
+        fetch("http://localhost:3000/users", options)
+            .then((res) => {
+                if(res.ok) {
+                    navigate("/dashboard")
+                }
+                
+            })
+            .catch((err) => console.log(err));
+    }
+
     return (
         <div className="signup-container">
-            <form action="#" method="post">
+            <form action={handleSubmit}>
                 <h1>TaskSphere</h1>
                 <LabeledInput
                     id="name"
                     label="Nome"
                     type="text"
                     placeholder="e.g. John Doe"
+                    value={userData.name}
+                    onChange={(value) => handleChange("name", value)}
                 />
 
                 <LabeledInput
@@ -20,6 +52,8 @@ function SignUpForm(props) {
                     label="Email"
                     type="email"
                     placeholder="e.g. teste@email.com"
+                    value={userData.email}
+                    onChange={(value) => handleChange("email", value)}
                 />
 
                 <LabeledInput
@@ -27,6 +61,8 @@ function SignUpForm(props) {
                     label="Senha"
                     type="password"
                     placeholder="Senha"
+                    value={userData.password}
+                    onChange={(value) => handleChange("password", value)}
                 />
 
                 <DefaultButton type="submit" text="Confirmar" />
