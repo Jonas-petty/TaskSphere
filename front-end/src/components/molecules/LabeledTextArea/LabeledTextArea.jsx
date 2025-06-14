097339fbf3c8a250965aa4b711cabdc8d1bb57cf
Jonas Felix <jonaspetty@hotmail.com>
2025-06-14 22:42:42 +0000

    fix: update content control on LabeledTextArea

--- front-end/src/components/molecules/LabeledTextArea/LabeledTextArea.jsx
+++ front-end/src/components/molecules/LabeledTextArea/LabeledTextArea.jsx
@@ -3,11 +3,16 @@ import TextArea from "../../atoms/Inputs/TextArea/TextArea";
 
 import "./LabeledTextArea.css";
 
-function LabeledTextArea({ id, label, placeholder }) {
+function LabeledTextArea({ id, label, placeholder, value, onChange }) {
     return (
         <div className="textarea-container">
             <Label for={id} text={label} />
-            <TextArea id={id} placeholder={placeholder} />
+            <TextArea
+                id={id}
+                placeholder={placeholder}
+                value={value}
+                onChange={onChange}
+            />
         </div>
     );
 }
