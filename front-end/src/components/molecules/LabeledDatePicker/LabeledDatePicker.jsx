ad20ee3922ab7878e7dad8b147883e892648da9b
Jonas Felix <jonaspetty@hotmail.com>
2025-06-14 22:44:02 +0000

    fix: update content control on LabeledDatePicker

--- front-end/src/components/molecules/LabeledDatePicker/LabeledDatePicker.jsx
+++ front-end/src/components/molecules/LabeledDatePicker/LabeledDatePicker.jsx
@@ -1,13 +1,18 @@
 import Label from "../../atoms/Labels/Label/Label";
 import DatePicker from "../../atoms/Inputs/DatePicker/DatePicker";
 
-import "./LabeledDatePicker.css"
+import "./LabeledDatePicker.css";
 
-function LabeledDatePicker({ id, label }) {
+function LabeledDatePicker({ id, label, value, onChange, minDate = "" }) {
     return (
         <div className="labeled-datepicker-container">
             <Label for={id} text={label} />
-            <DatePicker id={id} />
+            <DatePicker
+                id={id}
+                value={value}
+                onChange={onChange}
+                minDate={minDate}
+            />
         </div>
     );
 }
