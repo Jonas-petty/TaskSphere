83eef003c302fd102bb64d4bed299e9deb54ff3c
Jonas Felix <jonaspetty@hotmail.com>
2025-06-14 22:41:24 +0000

    feat: add content control and validation to TexArea

--- front-end/src/components/atoms/Inputs/TextArea/TextArea.jsx
+++ front-end/src/components/atoms/Inputs/TextArea/TextArea.jsx
@@ -1,12 +1,15 @@
-import "./TextArea.css"
+import "./TextArea.css";
 
-function TextArea({ id, placeholder }) {
+function TextArea({ id, placeholder, value, onChange }) {
     return (
         <textarea
-        className="textarea"
+            className="textarea"
             name={id}
             id={id}
             placeholder={placeholder}
+            value={value}
+            onChange={e => onChange(e.target.value)}
+            maxLength={500}
         ></textarea>
     );
 }
