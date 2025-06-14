910fc6b3c512d0c8d8618c3f75f326e3e42c23c2
Jonas Felix <jonaspetty@hotmail.com>
2025-06-14 22:40:44 +0000

    feat: add Input minLength validation

--- front-end/src/components/atoms/Inputs/Input/Input.jsx
+++ front-end/src/components/atoms/Inputs/Input/Input.jsx
@@ -4,11 +4,13 @@ function Input(props) {
 
     return (
         <input
+            className="input"
             id={props.id}
             type={props.type}
             placeholder={props.placeholder}
             value={props.value}
             onChange={(e) => props.onChange(e.target.value)}
+            minLength={3}
             required
         />
     );
