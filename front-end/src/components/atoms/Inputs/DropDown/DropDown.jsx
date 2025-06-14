12377a9f3776b1f2a3b44a083556a44388b64e83
Jonas Felix <jonaspetty@hotmail.com>
2025-06-14 18:34:56 +0000

    fix: make DropDown component complete overall
    The component was not working as expected, had to
    use third party solution

--- front-end/src/components/atoms/Inputs/DropDown/DropDown.jsx
+++ front-end/src/components/atoms/Inputs/DropDown/DropDown.jsx
@@ -1,22 +1,61 @@
-import "./DropDown.css";
+import Select from "react-select";
 
 function DropDown({ id, items, onClick }) {
-    items.sort((a, b) => {
-        return a.name.localeCompare(b.name)
-    });
+    const options = items.map((item) => ({
+        value: item.name,
+        label: item.name,
+    }));
 
-    const itemsList = items.map((item) => {
-        return (
-            <option key={item.id} value={item.name}>
-                {item.name}
-            </option>
-        );
+    const sortedOptions = [...options].sort((a, b) => {
+        return a.value.localeCompare(b.value);
     });
 
     return (
-        <select className="dropdown" name={id} id={id} onClick={onClick}>
-            {itemsList}
-        </select>
+        <Select
+            classNamePrefix="dropdown"
+            inputId={id}
+            isMulti={true}
+            options={sortedOptions}
+            styles={{
+                control: (baseStyles, state) => ({
+                    ...baseStyles,
+                    color: "#f4f7fe",
+                    borderColor: "#3f3f4b",
+                    backgroundColor: "#2c2c38",
+                    boxShadow: "none",
+                    ":hover": {
+                        borderColor: "#f4f7fe",
+                    },
+                }),
+                menuList: (baseStyles, state) => ({
+                    ...baseStyles,
+                    backgroundColor: "#2c2c38",
+                }),
+
+                option: (baseStyles, state) => ({
+                    ...baseStyles,
+                    color: "gray",
+                    backgroundColor: "#2c2c38",
+                    ":hover": {
+                        backgroundColor: "#3D3D49",
+                    },
+                }),
+                multiValue: (baseStyles, state) => ({
+                    ...baseStyles,
+                    backgroundColor: "#3D3D49",
+                    ":hover": {
+                        backgroundColor: "#3D3D49",
+                    },
+                }),
+                multiValueLabel: (baseStyles, state) => ({
+                    ...baseStyles,
+                    color: "gray",
+                    ":hover": {
+                        backgroundColor: "#3D3D49",
+                    },
+                }),
+            }}
+        />
     );
 }
 
